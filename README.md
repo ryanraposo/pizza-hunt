4fac982dd7e2cb11901307de9a7ed7ed4509d1c1
ryanraposo <raposo.ryan@gmail.com>
2023-07-06 06:20:22 -0400

    Implement and document local mongodb usage

---
+++ README.md
@@ -0,0 +1,9 @@
+# Pizza Hunt
+
+Spin up db:
+
+`mongod --dbpath ./data/db`
+
+Start:
+
+`npm run start`
--- server.js
+++ server.js
@@ -20,4 +20,4 @@ mongoose.set('debug', true);
 
 app.use(require('./routes'));
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Connected on http://localhost:${PORT}`));
